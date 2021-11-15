e9724b91d1caf9c8ae6f85d9cab2685d4728d08d
DaiNghia0212 <dainghia12311@gmail.com>
2021-11-15 13:55:28 +0700

    final

--- tasks.py
+++ tasks.py
@@ -1,7 +1,6 @@
 
 from configparser import ConfigParser
 from website import Website
-from website import browser
 from excel import getData
 from pdf import pdfData
 
@@ -9,21 +8,15 @@ parser = ConfigParser()
 parser.read("config.ini")
 
 
-# Define a main() function that calls the other functions in order:
-# 'xpath: //*[@id="node-23"]/div/div/div/div/div/div/div/a'
 def main():
     itdash_website = Website(url='https://itdashboard.gov/')
     try:
-        print('FlAG')
         itdash_website.open()
         itdash_website.click('//*[@href="#home-dive-in"]')
         itdash_website.get_agencies_infomation(
             agencies_info='id: agency-tiles-widget')
         agency = itdash_website.agencies[int(parser.get("Link",
                                                         "agency_index"))]
-        # agencyWebsite = data.Agency(
-        #     agency["link"], agency["name"],
-        #     table_info='id: investments-table-object')
         agency_website = Website(url=agency["link"])
         agency_website.open()
         agency_website.wait('id: investments-table-object')
